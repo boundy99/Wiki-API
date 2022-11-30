1d693e45319285ec2d89957dac5f5b760c49f1a3
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-11-30 16:25:39 -0500

    fetch data from database

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -5,11 +6,25 @@ const ejs = require('ejs');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
-mongoose.connect('mongodb://localhost:27017/wikiDB');
+mongoose.connect('mongodb://localhost:27017/WikiDB');
 
-const articlesSchema = {
+const articleSchema = {
     name: String,
+    content: String
 }
 
-const ARTICLES = mongoose.model('ARTICLES', articlesSchema);
+const Article = mongoose.model('Article', articleSchema);
+
+app.get('/articles', (req, res) => {
+    Article.find({}, (err, foundArticles) => {
+        if (!err) res.send(foundArticles)
+        else res.send(err)
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
